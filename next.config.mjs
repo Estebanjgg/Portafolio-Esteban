2bb4eafdc146d73d3238f91ae30d6b8e9a154175
Estebanjgg <esteban030991@gmail.com>
2025-01-24 21:09:31 -0300

    fix: deploy gitpages config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', 
+  basePath: '/portafolio-esteban', 
+  assetPrefix: '/portafolio-esteban/', 
 };
 
 export default nextConfig;
